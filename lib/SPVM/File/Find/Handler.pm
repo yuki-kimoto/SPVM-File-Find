dad6cbe38633be98ed69d768b4efed706bdaec72
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-23 08:38:48 +0900

    Added File::Find::Handler and implemented find

---
+++ lib/SPVM/File/Find/Handler.pm
@@ -0,0 +1,45 @@
+package SPVM::File::Find::Handler;
+
+
+
+1;
+
+=head1 Name
+
+SPVM::File::Find::Handler - Short Description
+
+=head1 Description
+
+C<SPVM::File::Find::Handler> is the C<File::Find::Handler> class in L<SPVM> language.
+
+=head1 Usage
+
+  use File::Find::Handler;
+
+=head1 Fields
+
+
+
+=head1 Class Methods
+
+
+
+=head1 Instance Methods
+
+
+
+=head1 Repository
+
+
+
+=head1 Author
+
+[--user-name] C<[--user-email]>
+
+=head1 Copyright & License
+
+Copyright 2023-2023 [--user-name], all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
