ad7545402c309e63189260af9a77e5190fe2988b
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-02-23 10:08:02 +0900

    Added the doc and the dependencies

--- lib/SPVM/File/Find.pm
+++ lib/SPVM/File/Find.pm
@@ -12,25 +12,61 @@ SPVM::File::Find - Short Description
 
 C<SPVM::File::Find> is the C<File::Find> class in L<SPVM> language.
 
+The C<File::Find> class has methods to get files under a directory.
+
 =head1 Usage
 
   use File::Find;
+  
+  my $dir = "lib";
+  
+  my $files_list = StringList->new;
+  
+  File::Find->find([$files_list : StringList] method : void ($dir : string, $file_base_name : string) {
+    my $file = $dir;
+    if ($file_base_name) {
+      $file .= "/$file_base_name";
+    }
+    
+    warn "$file";
+  }, $dir);
 
-=head1 Fields
+Gets file names:
 
-
+  use File::Find;
+  use StringList;
+  
+  my $dir = "lib";
+  
+  my $files_list = StringList->new;
+  
+  File::Find->find([$files_list : StringList] method : void ($dir : string, $file_base_name : string) {
+    my $file = $dir;
+    if ($file_base_name) {
+      $file .= "/$file_base_name";
+    }
+    
+    $files_list->push($file);
+    
+  }, $dir);
+  
+  my $files = $files_list->to_array;
 
 =head1 Class Methods
 
+  static method find : void ($cb : File::Find::Handler, $top_dir : string, $options = undef : object[]);
 
+Iterates each file recursively under the $top_dir and calls the $cb by the file.
 
-=head1 Instance Methods
+=head1 See also
 
+=head2 File::Find
 
+C<SPVM::File::Find> is a Perl's L<File::Find> porting to L<SPVM>.
 
 =head1 Repository
 
-
+L<SPVM::File::Find - Github|https://github.com/yuki-kimoto/SPVM-File-Find>
 
 =head1 Author
 
--- lib/SPVM/File/Find/Handler.pm
+++ lib/SPVM/File/Find/Handler.pm
@@ -6,40 +6,25 @@ package SPVM::File::Find::Handler;
 
 =head1 Name
 
-SPVM::File::Find::Handler - Short Description
+SPVM::File::Find::Handler - Handler for File::Find
 
 =head1 Description
 
 C<SPVM::File::Find::Handler> is the C<File::Find::Handler> class in L<SPVM> language.
 
+This class is the handler for L<find|SPVM::File::Find/"find"> method in the L<File::Find|SPVM::File::Find>.
+
 =head1 Usage
 
   use File::Find::Handler;
 
-=head1 Fields
+=head1 Interface Methods
 
-
-
-=head1 Class Methods
-
-
-
-=head1 Instance Methods
-
-
-
-=head1 Repository
-
-
-
-=head1 Author
-
-[--user-name] C<[--user-email]>
+  required method : void ($dir : string, $file_base_name : string);
 
 =head1 Copyright & License
 
-Copyright 2023-2023 [--user-name], all rights reserved.
+Copyright 2023-2023 Yuki Kimoto, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
-
