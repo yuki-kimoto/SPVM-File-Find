0307e622309d8218cc4164caf85dc178bf00e012
Yuki Kimoto <kimoto.yuki@gmail.com>
2025-05-09 15:03:33 +0900

    Add docs for SPVM::File::Find::Callback::Preprocess.

--- lib/SPVM/File/Find/Callback/Preprocess.pm
+++ lib/SPVM/File/Find/Callback/Preprocess.pm
@@ -6,32 +6,33 @@ package SPVM::File::Find::Callback::Preprocess;
 
 =head1 Name
 
-SPVM::File::Find::Callback::Preprocess - Short Description
+SPVM::File::Find::Callback::Preprocess - Callback for preprocess Option
 
 =head1 Description
 
-File::Find::Callback::Preprocess interface in L<SPVM> has interface methods to do someting.
+File::Find::Callback::Preprocess interface in L<SPVM> has an interface method for the callback method for C<preprocess> option.
 
 =head1 Usage
 
-  interface File::Find::Callback::Preprocess;
-
-=head1 Details
-
-
-
+  File::Find->new({
+    preproces => method : string[] ($files : string[]) {
+      
+    }
+  });
+  
 =head1 Interface Methods
 
+C<required method : string[] ($files : string[]);>
 
+An interface method for the callback method for C<preprocess> option.
 
+=head1 See Also
 
-=head1 Repository
+=over 2
 
+=item * L<File::Find|SPVM::File::Find>
 
-
-=head1 Author
-
-Yuki Kimoto C<kimoto.yuki@gmail.com>
+=back 
 
 =head1 Copyright & License
 
